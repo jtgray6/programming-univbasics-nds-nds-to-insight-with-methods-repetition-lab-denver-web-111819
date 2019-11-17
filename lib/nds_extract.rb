40cc3d2c9926b97209fbe64b946379c8a5bd91f8
Jeremy Gray <jeremygray@utexas.edu>
2019-11-17 00:40:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,37 +1,40 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
-require 'pp'
-require 'pry'
+# require 'pp'
+# require 'pry'
 
-pp directors_database
-binder.pry
+# pp directors_database
+# binder.pry
 
-# def directors_totals(source)
-#   result = {}
-#   director_index = 0
-#   while director_index < source.size do
-#     director = source[director_index]
-#     result[director[:name]] = gross_for_director(director)
-#     director_index += 1
-#   end
-#   result
-# end
+def directors_totals(source)
+  result = {}
+  director_index = 0
+  while director_index < source.size do
+    director = source[director_index]
+    result[director[:name]] = gross_for_director(director)
+    director_index += 1
+  end
+  result
+end
 
-# def gross_for_director(d)
-#   total = 0
-#   index = 0
+def gross_for_director(d)
+  total = 0
+  index = 0
 
-#   while index < d[:movies].length do
-#     total += d[:movies][index][:worldwide_gross]
-#     index += 1
-#   end
+  while index < d[:movies].length do
+    total += d[:movies][index][:worldwide_gross]
+    index += 1
+  end
 
-#   total
-# end
+  total
+end
 
-# def list_of_directors(source)
-#   dir_index=0
-# end
+def list_of_directors(source)
+  dir_index=0
+  list=[]
+  while dir_index < source.length
+    list[dir_index]=source[dir_index][:name]
+end
 
 def total_gross(source)
   # Write this implementation
